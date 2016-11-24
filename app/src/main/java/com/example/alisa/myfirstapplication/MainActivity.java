0b80935f7ce1f5fef3b752f649e5337e39360d38
alisabar <alisa.barsky@gmail.com>
2016-11-24 22:52:26 +0200

    Adding list.

--- app/src/main/java/com/example/alisa/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/alisa/myfirstapplication/MainActivity.java
@@ -4,14 +4,48 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView mainList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainList = (ListView) findViewById(R.id.btList);
 
+        // define data for the list
+        ArrayItem[] values = new ArrayItem[]{
+                new ArrayItem("Lecture 2: Calculator", CalculatorActivity.class),
+                new ArrayItem("Lecture 3: ConstraintLayoutA", ConstraintLayoutA.class),
+                new ArrayItem("Lecture 3: ConstraintLayoutB", ConstraintLayoutB.class),
+                new ArrayItem("Lecture 3: ConstraintLayoutC", ConstraintLayoutC.class),
+        };
+
+        // add the data to an adapter
+        final ArrayAdapter<ArrayItem> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, values);
+
+        // attach the data to the list view
+        mainList.setAdapter(adapter);
+
+        // wait for click events (this is a non-blocking call!)
+        mainList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                ArrayItem clicked = (ArrayItem) mainList.getItemAtPosition(position);
+                if (clicked.activity == null) {
+                    // nothing to do here
+                    Toast.makeText(getBaseContext(), "Activity is not attached", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent(getBaseContext(), clicked.activity);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     public void OpenCalculator(View view)
@@ -33,5 +67,22 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent intent = new Intent(this, ConstraintLayoutC.class);
         startActivity(intent);
+    }
+    static class ArrayItem {
+        private Class activity;
+        private String label;
+
+        public ArrayItem(String label, Class activity) {
+            this.label = label;
+            this.activity = activity;
+        }
+
+        @Override
+        public String toString() {
+            if (activity != null) {
+                return "Open " + label;
+            }
+            return label;
+        }
     }
 }
